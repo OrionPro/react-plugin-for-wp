666db3aad7ba3f5037248f82b072395f57426cc8
OrionPro <orion79@rambler.ru>
2023-03-30 12:48:04 +0300

    add data

---
+++ react-template.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Template Name: React App Template
+ *
+ *
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Givvy
+ */
+
+wp_head();
+the_content();
+wp_footer();
