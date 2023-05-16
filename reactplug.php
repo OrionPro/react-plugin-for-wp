d0c5939f38ff12a7781158786a360c68e7874ab5
OrionPro <orion79@rambler.ru>
2023-05-16 21:02:22 +0300

    update: reactplug.php and build files

--- reactplug.php
+++ reactplug.php
@@ -725,3 +725,38 @@ add_action( 'rest_api_init', 'create_ACF_meta_in_REST' );
 
 
 add_filter( 'wpcf7_validate_configuration', '__return_false' );
+
+/**
+ * SVG FIX
+ *
+ */
+
+add_filter('wp_check_filetype_and_ext', function ($data, $file, $filename, $mimes) {
+    $filetype = wp_check_filetype($filename, $mimes);
+    return [
+        'ext' => $filetype['ext'],
+        'type' => $filetype['type'],
+        'proper_filename' => $data['proper_filename']
+    ];
+
+}, 10, 4);
+
+function cc_mime_types_link_in_bio($mimes)
+{
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+}
+
+add_filter('upload_mimes', 'cc_mime_types_link_in_bio');
+
+function fix_svg_link_in_bio()
+{
+    echo '<style type="text/css">
+        .attachment-266x266, .thumbnail img {
+             width: 100% !important;
+             height: auto !important;
+        }
+        </style>';
+}
+
+add_action('admin_head', 'fix_svg_link_in_bio');
